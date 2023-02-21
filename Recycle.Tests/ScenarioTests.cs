0625c7977724092d96053d41f4b960f8aff39b49
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2023-02-21 11:38:22 +0100

    first 3 scenarios

---
+++ Recycle.Tests/ScenarioTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Recycle.WebAPI.Controllers;
+using Recycle.WebAPI.Messages;
+
+namespace Recycle.Tests;
+
+public class ScenarioTests
+{
+    private HandlingController _controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _controller = new HandlingController(Substitute.For<ILogger<HandlingController>>());
+    }
+
+    [Test]
+    public void NoFractionsDelivered()
+    {
+        var request = new RecycleRequest
+        {
+            Command = CalculatePrice(),
+            History = new List<Event>()
+        };
+
+        var evt = _controller.Handle(request) as Event<PriceWasCalculated>;
+
+        Assert.AreEqual(0, evt.Payload.PriceAmount);
+    }
+
+    [Test]
+    public void SingleFractionWasDelivered()
+    {
+        var request = new RecycleRequest
+        {
+            Command = CalculatePrice(),
+            History = new List<Event>
+            {
+                WeightWasMeasured(487),
+                new Event<FractionWasSelected>
+                {
+                    Payload = new FractionWasSelected
+                    {
+                        FractionType = "Construction waste"
+                    }
+                },
+                WeightWasMeasured(422)
+            }
+        };
+
+        var evt = _controller.Handle(request) as Event<PriceWasCalculated>;
+
+        Assert.AreEqual(9.75, evt.Payload.PriceAmount);
+    }
+
+    [Test]
+    public void MultipleFractionsDelivered()
+    {
+        var request = new RecycleRequest
+        {
+            Command = CalculatePrice(),
+            History = new List<Event>
+            {
+                WeightWasMeasured(487),
+                FractionWasSelected("Construction waste"),
+                WeightWasMeasured(422),
+                FractionWasSelected("Green waste"),
+                WeightWasMeasured(375)
+            }
+        };
+
+        var evt = _controller.Handle(request) as Event<PriceWasCalculated>;
+
+        Assert.AreEqual(13.98, evt.Payload.PriceAmount);
+    }
+
+    private static Command<CalculatePrice> CalculatePrice()
+    {
+        return new Command<CalculatePrice>
+        {
+            Payload = new CalculatePrice
+            {
+                CardId = "123"
+            }
+        };
+    }
+
+    private static Event<WeightWasMeasured> WeightWasMeasured(int weight)
+    {
+        return new Event<WeightWasMeasured>
+        {
+            Payload = new WeightWasMeasured
+            {
+                Weight = weight
+            }
+        };
+    }
+
+    private static Event<FractionWasSelected> FractionWasSelected(string fractionType)
+    {
+        return new Event<FractionWasSelected>
+        {
+            Payload = new FractionWasSelected
+            {
+                FractionType = fractionType
+            }
+        };
+    }
+}
--- Recycle.WebAPI/Controllers/HandlingController.cs
+++ Recycle.WebAPI/Controllers/HandlingController.cs
@@ -18,15 +18,62 @@ public class HandlingController
     [HttpPost]
     public Event Handle([FromBody] RecycleRequest request)
     {
-        logger.Log(LogLevel.Information, "/handle-command request => " + JsonSerializer.Serialize(request, JsonSerializationConfiguration.Default));
+        logger.Log(LogLevel.Information,
+            "/handle-command request => " + JsonSerializer.Serialize(request, JsonSerializationConfiguration.Default));
+
+        var measuredEvents = request.History
+            .Where(x => x.Type == "WeightWasMeasured")
+            .OrderBy(x => x.CreatedAt)
+            .Cast<Event<WeightWasMeasured>>()
+            .Select(x => x.Payload)
+            .ToArray();
+
+        var runningPrice = 0d;
+        var fractionPrice = 0d;
+        var previousWeight = 0d;
+        var firstWeight = true;
+        foreach (var evt in request.History)
+            if (evt.Type == nameof(WeightWasMeasured))
+            {
+                var wwm = evt as Event<WeightWasMeasured>;
+                if (firstWeight)
+                {
+                    firstWeight = false;
+                }
+                else
+                {
+                    var weightMeasured = previousWeight - wwm.Payload.Weight;
+                    runningPrice += weightMeasured * fractionPrice;
+                }
+
+                previousWeight = wwm.Payload.Weight;
+            }
+            else if (evt.Type == nameof(FractionWasSelected))
+            {
+                var fws = evt as Event<FractionWasSelected>;
+                fractionPrice = GetFractionPrice(fws);
+            }
+
         var response = new Event<PriceWasCalculated>
         {
             EventId = Guid.NewGuid().ToString(),
             CreatedAt = DateTime.Now,
-            Payload = new PriceWasCalculated { CardId = "123", PriceAmount = 0, PriceCurrency = "EUR" }
+            Payload = new PriceWasCalculated { CardId = "123", PriceAmount = runningPrice, PriceCurrency = "EUR" }
         };
 
-        logger.Log(LogLevel.Information, "/handle-command response => " + JsonSerializer.Serialize(response, JsonSerializationConfiguration.Default));
+        logger.Log(LogLevel.Information,
+            "/handle-command response => "
+            + JsonSerializer.Serialize(response, JsonSerializationConfiguration.Default));
         return response;
+    }
+
+    private static double GetFractionPrice(Event<FractionWasSelected> fws)
+    {
+        return fws.Payload.FractionType switch
+        {
+            "Construction waste" => 0.15,
+            "Green waste" => 0.09,
+            _ => 0
+        };
     }
 }
